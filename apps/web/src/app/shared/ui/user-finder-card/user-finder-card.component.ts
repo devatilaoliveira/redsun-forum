6f8cb80d0fe0ce8272e34a3c9e270007a30c0fc9
dev.atila.oliveira <dev.atila.oliveira@gmail.com>
2026-05-29 13:16:40 +0200

    Finishing up contact-about-card.component.html

--- apps/web/src/app/shared/ui/user-finder-card/user-finder-card.component.ts
+++ apps/web/src/app/shared/ui/user-finder-card/user-finder-card.component.ts
@@ -1,17 +1,20 @@
 import {Component, computed, input, InputSignal, output, OutputEmitterRef, Signal} from "@angular/core";
+import {RouterLink} from "@angular/router";
 import {TranslatePipe} from "@ngx-translate/core";
+import {ROUTE_PATHS} from "../../../../interface/constants/route-path.constants";
 import {UserFinderResultDTO} from "../../../../interface/dtos/user/UserFinderResultDTO";
 import {EVariant} from "../../../../interface/enums/EVariant";
 import {RsAvatar} from "../../fragments/rsAvatar/rs.avatar";
 import {RsBadge} from "../../fragments/rsBadge/rs.badge";
+import {RsBox} from "../../fragments/rsBox/rs.box";
 import {RsButton} from "../../fragments/rsButton/rs.button";
-import {RsDivider} from "../../fragments/rsDivider/rs.divider";
 import {RsMoreOption, RsMoreOptions} from "../../fragments/rsMoreOptions/rs.more-options";
+import {RsDivider} from "../../fragments/rsDivider/rs.divider";
 
 @Component({
   selector: "rs-user-finder-card",
   standalone: true,
-  imports: [TranslatePipe, RsAvatar, RsBadge, RsButton, RsDivider, RsMoreOptions],
+  imports: [RouterLink, TranslatePipe, RsAvatar, RsBadge, RsBox, RsButton, RsMoreOptions, RsDivider],
   templateUrl: "./user-finder-card.component.html",
   styleUrl: "./user-finder-card.component.scss"
 })
@@ -26,10 +29,14 @@ export class UserFinderCardComponent {
   public readonly menuOptionSelected: OutputEmitterRef<RsMoreOption> = output<RsMoreOption>();
 
   protected readonly EVariant = EVariant;
-  protected readonly roleCount: Signal<number> = computed<number>(() => this.user().favoriteRole?.length ?? 0);
-  protected readonly ruleCount: Signal<number> = computed<number>(() => this.user().favoriteRules?.length ?? 0);
-  protected readonly languageCount: Signal<number> = computed<number>(() => this.user().favoriteLanguage?.length ?? 0);
   protected readonly hasMenuOptions: Signal<boolean> = computed<boolean>(() => this.menuOptions().length > 0);
+  protected readonly profileRoute: Signal<string[]> = computed<string[]>(() => {
+    if (this.currentUser()) {
+      return ["/", ROUTE_PATHS.profileDetails];
+    }
+
+    return ["/", ROUTE_PATHS.contacts, ROUTE_PATHS.details, this.user().id];
+  });
 
   protected onAddContactPressed(): void {
     if (this.currentUser() || this.alreadyContact() || this.inProgress()) {
