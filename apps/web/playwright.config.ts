210914958516737fa3f32b9b3a4e14abf3df04d7
dev.atila.oliveira <dev.atila.oliveira@gmail.com>
2026-06-16 09:14:35 +0200

    minor fixes

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:local',
+    command: 'npm run start:stage',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env?.["CI"]
   }
